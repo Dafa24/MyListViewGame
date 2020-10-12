e4dd9c9afb5e0b673baf3cf23283f703af17323b
Dafa24 <akbarpradipa@gmail.com>
2020-10-12 10:03:31 +0700

    First Commit

---
+++ app/src/main/java/com/example/mylistview/Hero.kt
@@ -0,0 +1,10 @@
+package com.example.mylistview
+
+import android.app.ActivityManager
+import android.provider.ContactsContract
+
+data class Hero(
+    var photo: Int,
+    var name: String,
+    var description: String
+)
